db763104dc876d0b9c3b9ed88cf4f844494e19a1
ishandhanani <ishandhanani@gmail.com>
2025-05-09 17:55:39 -0700

    refactor(shell): remove git flag from eza aliases for better performance

--- common_rc/common_shell_config.sh
+++ common_rc/common_shell_config.sh
@@ -36,6 +36,6 @@ alias qq="cgpt -no-history"
 
 # eza
 alias ls='eza --color=always --group-directories-first'
-alias ll='eza -lh --color=always --git --group-directories-first'
-alias la='eza -la --color=always --git --group-directories-first' # All files
+alias ll='eza -lh --color=always --group-directories-first'
+alias la='eza -la --color=always --group-directories-first' # All files
 alias ltree='eza --tree --level=3 --color=always' # Tree view
